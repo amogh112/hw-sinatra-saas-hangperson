9df92786daea02427ca35c20eb6ae6d174d390b5
Amogh Gupta <amogh112@gmail.com>
2020-02-07 13:17:13 -0500

    Update hangperson_game.rb

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -8,20 +8,66 @@ class HangpersonGame
   # def initialize()
   # end
   
+  attr_accessor :word
+  attr_accessor :guesses
+  attr_accessor :wrong_guesses
+  
+  
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
   end
 
-  # You can test it by running $ bundle exec irb -I. -r app.rb
-  # And then in the irb: irb(main):001:0> HangpersonGame.get_random_word
-  #  => "cooking"   <-- some random word
   def self.get_random_word
     require 'uri'
     require 'net/http'
     uri = URI('http://watchout4snakes.com/wo4snakes/Random/RandomWord')
-    Net::HTTP.new('watchout4snakes.com').start { |http|
-      return http.post(uri, "").body
-    }
+    Net::HTTP.post_form(uri ,{}).body
+  end
+  
+  def guess(letter)
+    raise ArgumentError, "Guessed letter can't be nil" if letter == nil
+    raise ArgumentError, "Guessed letter can't be empty" if letter.empty?
+    raise ArgumentError, "Guessed letter must be a letter" unless letter.match(/[a-z]/i)
+    
+    letter_low = letter.downcase
+    # A letter that has already been guessed
+    # or is a non-alphabet character is considered "invalid"
+    if @guesses.include? letter_low or @wrong_guesses.include? letter_low or
+      letter_low.match(/[^a-z]/i)
+      valid = false
+    else
+      if @word.downcase.include? letter_low
+        @guesses += letter_low
+      else
+        @wrong_guesses += letter_low
+      end
+      valid = true
+    end
+    valid
+  end
+  
+  def word_with_guesses
+    retval = ''
+    @word.chars do |s|
+      if @guesses.include? s
+        retval += s
+      else
+        retval += '-'
+      end
+    end
+    retval
+  end
+  
+  def check_win_or_lose
+    check = :play
+    if word_with_guesses == @word and @wrong_guesses.length < 7
+      check = :win
+    elsif @wrong_guesses.length >= 7
+      check = :lose
+    end
+    check
   end
 
 end
